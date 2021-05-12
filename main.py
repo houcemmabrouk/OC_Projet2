40911e7057f25bf607bc4444a59dd89fd59632bd
Houcem Mabrouk <houcem.mabrouk@gmail.com>
2021-05-12 13:34:03 +0100

    [remplacement du code par une serie de fonctions]

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ all_product_info = {}
 
 
 # scraper returns data for specific soup givin a specifc HTML tag to a specific Sublevel
-def scraper(level, HTMLtag, soup):
+def scraper(level, html_tag, soup):
     for levelCounter in range(level):
-        soup = soup.findNext(HTMLtag)
+        soup = soup.findNext(html_tag)
     return soup.text
 
 
@@ -98,79 +98,7 @@ urls_store = generate_all_books_url(all_category_pages)
 #print(urls_store)
 #print(len(urls_store))
 
-def scrap_write(url_list):
-    with open("scrap.csv", "w") as csvfile:
-        fieldnames = ["internal_id", "product_page_url", "title", "product_description", "category", "image_url",
-                      "universal_product_code", "price_excluding_tax", "price_including_tax",
-                      "number_available", "review_rating"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        productCount = 0
-        for url in url_list:
-            #print(url)
-            response = requests.get(url)
-            if response.ok:
-                productCount = productCount + 1
-                #print(productCount)
-                soup = BeautifulSoup(response.text, "html.parser")
-                # Add internal_id to productInfo
-                internal_id = str(productCount)
-                productInfo.update({ "internal_id" : internal_id})
 
-                # Add product_page_url to productInfo
-                product_page_url = url
-                productInfo.update({"product_page_url" : url})
-
-                # Add title to productInfo
-                title = utf8Format(scraper(0, "title", soup.find("h1")))
-                productInfo.update({"title": title})
-
-                # Add product_description to productInfo
-                product_description = utf8Format((scraper(1, "p", soup.find("h2"))))
-                productInfo.update({"product_description": product_description})
-
-                # Add category to productInfo
-                category = utf8Format((scraper(3, "a", soup.find("ul"))))
-                productInfo.update({"category": category})
-
-                # Add image_url to productInfo
-                incomplete_image_url = (soup.find("article").find("img")["src"])
-                image_url = incomplete_image_url.replace("../..","http://books.toscrape.com")
-                productInfo.update({"image_url": image_url})
-
-                # Add universal_product_code to productInfo
-                universal_product_code = (scraper(1, "td", soup.find("table").find("tr")))
-                productInfo.update({"universal_product_code": universal_product_code})
-
-                # Add price_excluding_tax to productInfo
-                price_excluding_tax = utf8Format((scraper(3, "td", soup.find("table").find("tr"))))
-                productInfo.update({"price_excluding_tax": price_excluding_tax})
-
-                # Add price_including_tax to productInfo
-                price_including_tax = utf8Format((scraper(4, "td", soup.find("table").find("tr"))))
-                productInfo.update({"price_including_tax": price_including_tax})
-
-                # Add number_available to productInfo
-                number_available = utf8Format((scraper(6, "td", soup.find("table").find("tr"))))
-                productInfo.update({"number_available": number_available})
-
-                # Add review_rating to productInfo
-                review_rating = utf8Format((scraper(7, "td", soup.find("table").find("tr"))))
-                productInfo.update({"review_rating": review_rating})
-
-                writer.writerow({"internal_id": productInfo["internal_id"],
-                                 "title": productInfo["title"],
-                                 "product_page_url": productInfo["product_page_url"],
-                                 "product_description": productInfo["product_description"],
-                                 "category": productInfo["category"],
-                                 "image_url": productInfo["image_url"],
-                                 "universal_product_code": productInfo["universal_product_code"],
-                                 "price_excluding_tax": productInfo["price_excluding_tax"],
-                                 "price_including_tax": productInfo["price_including_tax"],
-                                 "number_available": productInfo["number_available"],
-                                 "review_rating": productInfo["review_rating"]
-                                 })
-    csvfile.close()
 
 
 #laisser scrap_write pour le moment
